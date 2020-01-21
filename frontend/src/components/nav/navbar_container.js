7f1275912bc9aec7b275c3a030639eddc3527ca1
linlinliu <linlinliu03@gmail.com>
2020-01-21 13:48:33 -0800

    Continue to work on search

--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,6 +1,8 @@
 import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
 import {fetchSearchMeals} from '../../actions/meal_actions';
+import { logout, login } from '../../actions/session_actions';
+import { openModal } from '../../actions/modal_actions';
+
 
 import NavBar from './navbar';
 
@@ -10,7 +12,9 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     logout: () => dispatch(logout()),
-    fetchSearchMeals: (keyword) => dispatch(fetchSearchMeals(keyword))
+    fetchSearchMeals: (keyword) => dispatch(fetchSearchMeals(keyword)),
+    openModal: modal => dispatch(openModal(modal)),
+    demoLogin: (demoUser) => dispatch(login(demoUser))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(NavBar);
