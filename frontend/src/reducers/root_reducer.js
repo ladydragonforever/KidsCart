f16a847fc00dbc1a73ccb10ba8e588fc9d6c6572
linlinliu <linlinliu03@gmail.com>
2020-01-20 21:19:45 -0800

    finish the frontend react redux cycle for search

--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import session from './session_reducer';
 import errors from './errors_reducer';
+import meals from './meals_reducer';
 
 const RootReducer = combineReducers({
     errors,
     session,
+    meals
 });
 
 export default RootReducer;
